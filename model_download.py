6bdcaf6f4e6236dcb280c605711128c365d00343
yehengchen <cyh627955504@gmail.com>
2024-07-10 15:53:38 +0800

    20240710

--- model_download.py
+++
@@ -1,5 +0,0 @@
-import torch
-from modelscope import snapshot_download, AutoModel, AutoTokenizer
-import os
-
-model_dir = snapshot_download('LLM-Research/Meta-Llama-3-8B-Instruct', cache_dir='/home/zjlab/models', revision='master')
--- model_download2.py
+++
@@ -1,5 +0,0 @@
-import torch
-from modelscope import snapshot_download, AutoModel, AutoTokenizer
-import os
-
-model_dir = snapshot_download('Gurveer05/blip-image-captioning-base-rscid-finetuned', cache_dir='/home/mars/cyh_ws/LLM/models', revision='master')
